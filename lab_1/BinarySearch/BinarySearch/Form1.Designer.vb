58d91649a735cf1d9ed7554607cdace6eaf03665
Sunny Kumar <sunny170101068@iitg.ac.in>
2019-07-25 10:47:05 +0530

    Add files via upload

---
+++ lab_1/BinarySearch/BinarySearch/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class binSearch
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.newVal = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.arrSize = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.xFind = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.arrStatus = New System.Windows.Forms.TextBox()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.result = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LowHighPos = New System.Windows.Forms.TextBox()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.lblFile = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'newVal
+        '
+        Me.newVal.Location = New System.Drawing.Point(1031, 39)
+        Me.newVal.Name = "newVal"
+        Me.newVal.Size = New System.Drawing.Size(130, 26)
+        Me.newVal.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(589, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(391, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter Value To Be Added In The Array:"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(1231, 28)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(194, 42)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "ADD"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'arrSize
+        '
+        Me.arrSize.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.arrSize.Location = New System.Drawing.Point(1032, 105)
+        Me.arrSize.Name = "arrSize"
+        Me.arrSize.ReadOnly = True
+        Me.arrSize.Size = New System.Drawing.Size(129, 26)
+        Me.arrSize.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(589, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(344, 25)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Number Of Elements In The Array:"
+        '
+        'xFind
+        '
+        Me.xFind.Location = New System.Drawing.Point(1033, 197)
+        Me.xFind.Name = "xFind"
+        Me.xFind.Size = New System.Drawing.Size(128, 26)
+        Me.xFind.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(588, 191)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(410, 32)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Enter Value To Be Searched:"
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(1231, 99)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(194, 41)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "CLEAR"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Location = New System.Drawing.Point(1184, 178)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(241, 61)
+        Me.btnSearch.TabIndex = 8
+        Me.btnSearch.Text = "SEARCH"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 374)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(574, 25)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Status Of The Array After Each Iteration Of Binary Search:"
+        '
+        'arrStatus
+        '
+        Me.arrStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.arrStatus.Location = New System.Drawing.Point(17, 430)
+        Me.arrStatus.Name = "arrStatus"
+        Me.arrStatus.ReadOnly = True
+        Me.arrStatus.Size = New System.Drawing.Size(1408, 23)
+        Me.arrStatus.TabIndex = 10
+        '
+        'btnNext
+        '
+        Me.btnNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNext.Location = New System.Drawing.Point(736, 363)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(689, 44)
+        Me.btnNext.TabIndex = 11
+        Me.btnNext.Text = "NEXT"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'result
+        '
+        Me.result.Location = New System.Drawing.Point(1031, 273)
+        Me.result.Name = "result"
+        Me.result.ReadOnly = True
+        Me.result.Size = New System.Drawing.Size(394, 26)
+        Me.result.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(905, 267)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(111, 32)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Result:"
+        '
+        'LowHighPos
+        '
+        Me.LowHighPos.BackColor = System.Drawing.Color.Khaki
+        Me.LowHighPos.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LowHighPos.ForeColor = System.Drawing.Color.Indigo
+        Me.LowHighPos.Location = New System.Drawing.Point(17, 462)
+        Me.LowHighPos.Name = "LowHighPos"
+        Me.LowHighPos.ReadOnly = True
+        Me.LowHighPos.Size = New System.Drawing.Size(1408, 23)
+        Me.LowHighPos.TabIndex = 14
+        '
+        'btnSelect
+        '
+        Me.btnSelect.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSelect.Location = New System.Drawing.Point(81, 246)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(140, 42)
+        Me.btnSelect.TabIndex = 15
+        Me.btnSelect.Text = "SELECT"
+        Me.btnSelect.UseVisualStyleBackColor = True
+        '
+        'lblFile
+        '
+        Me.lblFile.AutoSize = True
+        Me.lblFile.Location = New System.Drawing.Point(85, 312)
+        Me.lblFile.Name = "lblFile"
+        Me.lblFile.Size = New System.Drawing.Size(125, 20)
+        Me.lblFile.TabIndex = 16
+        Me.lblFile.Text = "No File Selected"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(75, 191)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(312, 32)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Select Input Text File:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.PaleGreen
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(46, 67)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(481, 64)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "BINARY SEARCH"
+        '
+        'binSearch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.Color.Bisque
+        Me.ClientSize = New System.Drawing.Size(1437, 512)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblFile)
+        Me.Controls.Add(Me.btnSelect)
+        Me.Controls.Add(Me.LowHighPos)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.result)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.arrStatus)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.xFind)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.arrSize)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.newVal)
+        Me.Name = "binSearch"
+        Me.Text = "Binary Search"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents newVal As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents arrSize As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents xFind As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents arrStatus As System.Windows.Forms.TextBox
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+    Friend WithEvents result As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents LowHighPos As System.Windows.Forms.TextBox
+    Friend WithEvents btnSelect As System.Windows.Forms.Button
+    Friend WithEvents lblFile As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ lab_1/BinarySearch/BinarySearch/Form1.vb
@@ -0,0 +1,416 @@
+﻿Public Class binSearch
+
+    Dim arr(1000000) As Double
+    ' "arr" is the array that stores all the number values in sorted order
+
+    Dim sz As Integer = 0
+    ' "sz" is an integer variable that stores the size of array "arr"
+
+    Dim x As Double
+    ' "x" is a variable to store the value that has to be searched
+
+    Dim val As Double
+    ' "val" stores the input value that will be added to the array
+
+    Dim posLow(100), posMid(100), posHigh(100) As Integer
+    ' "posLow" is an array to store the position of "L" at each iteration of Binary Search
+
+    Dim cnt As Integer = 0
+    ' "cnt" the number iterations the "do while" loop ran to find "x"
+
+    Dim temp As Integer = 0
+    ' "temp" is temporary variable for sometimes use
+
+    Dim brr(1000000) As String
+    ' "brr" is the array that stores all the strings in sorted order
+
+    Dim crr() As String
+    ' "crr" array is used to read the input from file
+
+    Dim type As Integer = -1
+    ' Stores the type of input, if input is a real number then type=1, else in the case of string type=2
+
+    Dim xString As String = ""
+    ' "xString" is temporary string variable for sometimes use
+
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+
+        ' "Double.TryParse(newVal.Text, val)" will return TRUE if the string newVal.Text is also a valid 
+        ' representation of a double number and FALSE otherwise, in the case of TRUE it will also store 
+        ' the double number corresponding to that string into double variable "val"
+
+        If (Double.TryParse(newVal.Text, val) = True) And (type <> 2) Then
+            ' If input is a real number
+
+            type = 1
+
+            If sz = 0 Then
+                ' Trivial condition checked
+                arr(sz) = val
+                sz += 1         ' array size increases by 1
+
+            ElseIf arr(sz - 1) <= val Then
+                ' Array must be sorted, hence checking the sorting condition
+                arr(sz) = val
+                sz += 1         ' array size increases by 1
+
+            Else
+                ' User is informed about invalid input
+                MessageBox.Show("Entered element disrupts the Sorted nature of array.", "INVALID INPUT")
+
+            End If
+
+        Else
+
+            If (type = 1) Then
+                ' User is informed about invalid input
+                MessageBox.Show("Enter a Integer or a Real Number.", "INVALID INPUT")
+            Else
+                If (Double.TryParse(newVal.Text, val) = True) Then
+                    ' User is informed about invalid input
+                    MessageBox.Show("Enter a String.", "INVALID INPUT")
+                Else
+                    If sz = 0 Then
+                        ' Trivial condition checked
+                        brr(sz) = newVal.Text
+                        type = 2
+                        sz += 1         ' array size increases by 1
+
+                    ElseIf String.Compare(brr(sz - 1), newVal.Text) <= 0 Then
+                        ' Array must be sorted, hence checking the sorting condition
+                        brr(sz) = newVal.Text
+                        type = 2
+                        sz += 1         ' array size increases by 1
+
+                    Else
+                        ' User is informed about invalid input
+                        MessageBox.Show("Entered element disrupts the Sorted nature of array.", "INVALID INPUT")
+
+                    End If
+                End If
+            End If
+
+        End If
+
+        arrSize.Text = sz.ToString()    'Printing the new array size
+
+        ' Information about previous states is no longer required after the addition of a new element to array
+        newVal.Text = ""
+        xFind.Text = ""
+        result.Text = ""
+        arrStatus.Text = ""
+        For i As Integer = 0 To (cnt - 1)
+            posLow(i) = 0
+            posMid(i) = 0
+            posHigh(i) = 0
+        Next
+        cnt = 0
+        temp = 0
+        LowHighPos.Text = ""
+
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+
+        ' Clear Button makes every integer or double variable as zero and strings as empty string 
+        For i As Integer = 0 To (sz - 1)
+            arr(i) = 0
+            brr(i) = ""
+        Next
+        sz = 0
+        newVal.Text = ""
+        arrSize.Text = ""
+        xFind.Text = ""
+        result.Text = ""
+        arrStatus.Text = ""
+        For i As Integer = 0 To (cnt - 1)
+            posLow(i) = 0
+            posMid(i) = 0
+            posHigh(i) = 0
+        Next
+        cnt = 0
+        temp = 0
+        type = -1
+        LowHighPos.Text = ""
+        lblFile.Text = "No File Selected"
+
+    End Sub
+
+    Private Sub btnSearch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSearch.Click
+
+        If xFind.Text = "" Then
+            ' User is informed about invalid input
+            MessageBox.Show("Enter the Element to be searched.", "ERROR")
+
+        ElseIf Double.TryParse(xFind.Text, val) = False Then
+            If (type = 1) Then
+                ' User is informed about invalid input
+                MessageBox.Show("Enter a String.", "INVALID INPUT")
+            Else
+                ' Standard Binary Search Algorithm
+                xString = xFind.Text
+                Dim low As Integer = 0, high As Integer = sz - 1
+                Dim mid As Integer
+
+                ' Forgetting the information about the previous states 
+                For i As Integer = 0 To (cnt - 1)
+                    posLow(i) = 0
+                    posMid(i) = 0
+                    posHigh(i) = 0
+                Next
+                cnt = 0
+                temp = 0
+                LowHighPos.Text = ""
+                result.Text = "NOT FOUND"
+
+                Do While (low < high)
+
+                    mid = Fix((low + high) / 2)
+
+                    ' Storing the value of low, mid and high during each iteration of Bianry Search
+                    posLow(cnt) = low
+                    posMid(cnt) = mid
+                    posHigh(cnt) = high
+                    cnt += 1
+
+                    If String.Compare(brr(mid), xString) = 0 Then
+                        ' If xString matches with middle element, then we make low > high, so that the loop breaks
+                        low = mid + 1
+                        high = mid - 1
+
+                    ElseIf String.Compare(brr(mid), xString) > 0 Then
+                        'If xString < brr(mid) element, then xString can only lie in left half subarray after the mid element.
+                        high = mid - 1
+
+                    Else
+                        'If xString > brr(mid) element, then xString can only lie in right half subarray after the mid element.
+                        low = mid + 1
+
+                    End If
+
+                Loop
+
+                ' last iteration
+                mid = Fix((low + high) / 2)
+                If String.Compare(brr(mid), xString) = 0 Then
+
+                    posLow(cnt) = mid
+                    posMid(cnt) = mid
+                    posHigh(cnt) = mid
+                    cnt += 1
+                    result.Text = "FOUND"
+
+                End If
+            End If
+
+        ElseIf sz = 0 Then
+            ' User is informed about invalid input
+            MessageBox.Show("Array is empty right now.", "ERROR")
+
+        Else
+            ' Standard Binary Search Algorithm
+            x = val
+            Dim low As Integer = 0, high As Integer = sz - 1
+            Dim mid As Integer
+
+            ' Forgetting the information about the previous states 
+            For i As Integer = 0 To (cnt - 1)
+                posLow(i) = 0
+                posMid(i) = 0
+                posHigh(i) = 0
+            Next
+            cnt = 0
+            temp = 0
+            LowHighPos.Text = ""
+            result.Text = "NOT FOUND"
+
+            Do While (low < high)
+
+                mid = Fix((low + high) / 2)
+
+                ' Storing the value of low, mid and high during each iteration of Bianry Search
+                posLow(cnt) = low
+                posMid(cnt) = mid
+                posHigh(cnt) = high
+                cnt += 1
+
+                If arr(mid) = x Then
+                    ' If x matches with middle element, then we make low > high, so that the loop breaks
+                    low = mid + 1
+                    high = mid - 1
+
+                ElseIf arr(mid) > x Then
+                    'If x < arr(mid) element, then x can only lie in left half subarray after the mid element.
+                    high = mid - 1
+
+                Else
+                    'If x > arr(mid) element, then x can only lie in right half subarray after the mid element.
+                    low = mid + 1
+
+                End If
+
+            Loop
+
+            ' last iteration
+            mid = Fix((low + high) / 2)
+            If arr(mid) = x Then
+
+                posLow(cnt) = mid
+                posMid(cnt) = mid
+                posHigh(cnt) = mid
+                cnt += 1
+                result.Text = "FOUND"
+
+            End If
+
+            End If
+
+    End Sub
+
+    Private Sub btnNext_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnNext.Click
+
+        ' Complete array output
+        arrStatus.Text = ""
+        If type <> 2 Then
+            For i As Integer = 0 To (sz - 1)
+                arrStatus.Text += arr(i).ToString()
+                arrStatus.Text += vbTab
+            Next
+        Else
+            For i As Integer = 0 To (sz - 1)
+                arrStatus.Text += brr(i)
+                arrStatus.Text += vbTab
+            Next
+        End If
+
+        LowHighPos.Text = ""
+
+        If sz = 0 Then
+            ' User is informed about invalid operation
+            MessageBox.Show("Array is empty.", "ERROR")
+
+        ElseIf sz = 1 Then
+
+        ElseIf cnt = 0 Then
+            ' User is informed about invalid operation
+            MessageBox.Show("First Search an element.", "ERROR")
+
+        Else
+            ' Printing L, M, H 
+            For i As Integer = 0 To (sz - 1)
+                ' temp = current iteration of binary search
+                If posLow(temp) = i Then
+                    LowHighPos.Text += "L"
+                End If
+                If posMid(temp) = i Then
+                    LowHighPos.Text += "M"
+                End If
+                If posHigh(temp) = i Then
+                    LowHighPos.Text += "H"
+                End If
+                LowHighPos.Text += vbTab
+            Next
+
+            ' "cnt" is the total number of iterations of binary search
+            If temp < cnt - 1 Then
+                temp += 1
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub btnFile_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSelect.Click
+
+        ' Calling for CLEAR BUTTON
+        btnClear_Click(sender, e)
+
+        OpenFileDialog1.Title = "SELECT TEXT FILE"
+        OpenFileDialog1.ShowDialog()
+        ' Storing the name of input file in lblFile.Text
+        lblFile.Text = OpenFileDialog1.FileName
+
+        If lblFile.Text.Substring(lblFile.Text.Length - 4, 4) = ".txt" Then
+            ' Checking if the file is a text file or not
+
+            crr = IO.File.ReadAllLines(lblFile.Text)
+            ' "crr" now stores all data of input file, with each line of input file as a separate string in "crr" 
+
+            ' Checking the type of inputs given
+            sz = UBound(crr) + 1
+            If sz <> 0 Then
+                If Double.TryParse(crr(0), val) = True Then
+                    type = 1
+                Else
+                    type = 2
+                End If
+            End If
+            sz = 0
+
+            Dim wrong As Integer = 0
+            ' If wrong =1, then the inputs are invalid
+
+            For i As Integer = 0 To UBound(crr)
+                ' Running a loop from staring to the end of the array "crr"
+
+                ' If the inputs are invalid, break the FOR loop
+                If wrong = 1 Then
+                    Exit For
+                End If
+
+                ' This is same as ADD BUTTON
+                If type <> 2 Then
+                    If Double.TryParse(crr(i), val) = True Then
+
+                        If sz = 0 Then
+                            arr(sz) = val
+                            sz += 1
+                        ElseIf arr(sz - 1) <= val Then
+                            arr(sz) = val
+                            sz += 1
+                        Else
+                            MessageBox.Show("Enter sorted array.", "INVALID INPUT")
+                            wrong = 1
+                        End If
+                    Else
+                        MessageBox.Show("Enter Integers or Real Numbers only.", "INVALID INPUT")
+                        wrong = 1
+                    End If
+                Else
+                    If Double.TryParse(crr(i), val) = False Then
+
+                        If sz = 0 Then
+                            brr(sz) = crr(i)
+                            sz += 1
+                        ElseIf String.Compare(brr(sz - 1), crr(i)) <= 0 Then
+                            brr(sz) = crr(i)
+                            sz += 1
+                        Else
+                            MessageBox.Show("Enter sorted array.", "INVALID INPUT")
+                            wrong = 1
+                        End If
+                    Else
+                        MessageBox.Show("Enter strings only.", "INVALID INPUT")
+                        wrong = 1
+                    End If
+                End If
+            Next
+
+            ' Updating the size of "arr"
+            arrSize.Text = sz.ToString()
+
+            If wrong = 1 Then
+                ' Since, wrong =1, so the there is some invalid input, so we call CLEAR BUTTON to bring the application back to intial states
+                btnClear_Click(sender, e)
+
+            End If
+
+        Else
+            ' User is informed about invalid format of file, and the application is brought to its intial staes
+            MessageBox.Show("Select a Text file.", "ERROR")
+            btnClear_Click(sender, e)
+        End If
+
+    End Sub
+
+End Class
